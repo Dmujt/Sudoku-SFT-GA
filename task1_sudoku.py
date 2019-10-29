04836356c30af8470db62101ec71be8d569faf5e
Dena Mujtaba <mujtdena@gmail.com>
2019-10-29 16:50:29 -0400

    Inital commit

---
+++ task1_sudoku.py
@@ -0,0 +1,13 @@
+#####################################
+# GA for Sudoku
+# Dena Mujtaba
+#####################################
+
+# SETUP 
+# tournament selection
+
+#main class to solve the 
+class Sudoku():
+
+    def __init__(self):
+        print("here")
