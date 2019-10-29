f51f40e57f815be95b0949befd4651d7f3d8e374
Dena Mujtaba <mujtdena@gmail.com>
2019-10-29 18:21:46 -0400

    Class structure for GA

--- task1_sudoku.py
+++ task1_sudoku.py
@@ -1,13 +1,37 @@
 #####################################
-# GA for Sudoku
+# Task 1: GA for Sudoku
 # Dena Mujtaba
 #####################################
 
-# SETUP 
-# tournament selection
+# SETUP PARAMS
+populationSize = 100
+numGenerations = 10
+
+# initialize the population
+def populationGeneration():
+    return []
 
 #main class to solve the 
-class Sudoku():
+class SudokuGA():
 
+    #constructor for GA
     def __init__(self):
-        print("here")
+        # check arguments for GA setup parameters
+        print("Starting...")
+
+    # will return child
+    def crossover(self, p1, p2):
+        return p1
+
+    # mutate x
+    def mutate(self, x):
+        return x
+
+    def fitness(self):
+        return 0
+    
+    def select(self):
+        return 0
+
+if __name__ == "__main__":
+    population = []
---
+++ task2_santaFeTrail.py
@@ -0,0 +1,18 @@
+#####################################
+# Task 2: GP for Santa Fe Ant Trail Problem
+# Dena Mujtaba
+#####################################
+
+# SETUP 
+# tournament selection
+
+#main class to solve the 
+class SantaGP():
+
+    #constructor for GA
+    def __init__(self):
+        # check arguments for GP setup parameters
+        print("Starting...")
+
+if __name__ == "__main__":
+    SantaGP()
